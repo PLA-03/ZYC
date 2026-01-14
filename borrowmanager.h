ba2b95324c3720c86a76a0f64ec62603bbb3e8c7
sweetilekarag@outlook.com <sweetilekarag@outlook.com>
2026-01-14 19:22:14 +0800

    完成借阅管理模块，上传.h文件

--- borrowmanager.h
+++ borrowmanager.h
@@ -18,13 +18,7 @@ public:
     bool borrowBook(const QString& bookId, const QString& readerId, const QDate& borrowDate, const QDate& dueDate);
     bool returnBook(int borrowId, const QDate& returnDate);
     
-    // New methods as per user requirements
-    bool checkOutBook(const QString& bookId, const QString& readerId);
-    bool checkInBook(const QString& borrowId);
-    QList<QMap<QString, QVariant>> getOverdueRecords();
-    
     QSqlQueryModel* getAllBorrows();
-    QSqlQueryModel* getActiveBorrows();
     
     bool exportToCSV(const QString& filePath);
     
